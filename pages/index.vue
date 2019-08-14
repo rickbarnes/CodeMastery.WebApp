ff5245bd9e61f601ca4112d3f1200785ca618114
Rick Barnes <rick.barnes@fivesixtwo.com>
2019-08-14 09:12:53 -0700

    test commit from work laptop

--- pages/index.vue
+++ pages/index.vue
@@ -4,6 +4,7 @@
       <logo />
       <h1 class="title">CodeMastery.WebApp</h1>
       <h2 class="subtitle">The WebApp for CodeMastery</h2>
+      <p>Test</p>
       <div class="links">
         <!-- <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
         <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>-->
