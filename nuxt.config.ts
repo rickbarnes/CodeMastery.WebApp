3245938d5c45bb7a822b0133dc63fc2a8f39489b
Rick Barnes <rbarnes213@gmail.com>
2019-08-13 20:22:43 -0700

    fixes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,7 @@ const config: NuxtConfiguration = {
    ** Headers of the page
    */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'CodeMastery' || '',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
